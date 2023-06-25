ec38a6bea198942fdd21732eb5cb8e7237c271ef
Furkan Kalkan <furkan.selim1999@hotmail.com>
2023-06-25 23:49:29 +0200

    Update ensembling.py

--- ensembling.py
+++ ensembling.py
@@ -2,6 +2,8 @@ import os
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
+from collections import Counter
+
 from log.Ualexnet.model import ImageClassifier
 from log.Calexnet.model import ImageClassifier2
 from log.Uresnet50.model import ImageClassifier3
@@ -23,7 +25,6 @@ preprocess = transforms.Compose([
 
 # Path to the directory containing the images
 image_dir = "../../data for emotion classification/dataset_cleaned/manual_testing/relaxed"
-#image_dir = "../../data for emotion classification/dataset_cleaned/test/relaxed"
 
 # List of model classes, their corresponding file paths, and softmax functions
 models = [
@@ -34,16 +35,17 @@ models = [
     {"class": ImageClassifier5(num_classes=4), "path": "log/Calexnet100Epochs/alexnet100.pth", "name": "Cleaned Alexnet100"},
 ]
 
-correct_predictions = 0
-total_images = 0
-
 if len(models) == 0:
     print("No models found. Please check the 'models' list.")
     exit()
 
 # Iterate over the images and perform predictions
 print(f"Using device: {device}")
+correct_predictions = 0
+total_images = 0
+
 for root, dirs, files in os.walk(image_dir):
+    predictions = []
     for image_file in files:
         image_path = os.path.join(root, image_file)
 
@@ -53,10 +55,9 @@ for root, dirs, files in os.walk(image_dir):
         input_image = input_image.unsqueeze(0)  # Add a batch dimension
         input_image = input_image.to(device)
 
-        # Calculate average confidence values for each model
-        average_confidence_values = [0] * len(models)
-
-        for model_idx, model_info in enumerate(models):
+        # Make predictions for each model
+        model_predictions = []
+        for model_info in models:
             model_class = model_info["class"]
             model_path = model_info["path"]
             model_name = model_info["name"]
@@ -73,41 +74,38 @@ for root, dirs, files in os.walk(image_dir):
                 softmax = torch.nn.Softmax(dim=1)
                 probabilities = softmax(output)
 
-            # Calculate the confidence value for each predicted class
-            confidence_values = probabilities[0].tolist()
-
-            # Update average confidence values
-            for i, confidence in enumerate(confidence_values):
-                average_confidence_values[i] += confidence
-
             # Get the predicted label
             predicted_index = torch.argmax(probabilities, dim=1).item()
             predicted_label = class_labels[predicted_index]
+
+            # Add the predicted label to the list
+            model_predictions.append(predicted_label)
 
             # Print the predicted label and confidence values for the current model
             print(f"Model: {model_name}")
             print(f"Predicted: {predicted_label}")
             print(f"Image: {image_file}")
             for i, emotion in enumerate(class_labels):
-                print(f"{emotion}: {confidence_values[i] * 100:.2f}%")
+                print(f"{emotion}: {probabilities[0][i] * 100:.2f}%")
             print("---------------------------")
 
-        # Determine the final prediction based on the highest average confidence value
-        max_average_confidence = max(average_confidence_values)
-        final_prediction = class_labels[average_confidence_values.index(max_average_confidence)]
+        # Store the predictions for the image
+        predictions.append(model_predictions)
 
-        ground_truth_label = os.path.basename(root)
-        if (final_prediction == ground_truth_label):
-            correct_predictions +=1
+    # Determine the final prediction based on the most common prediction among all models
+    final_predictions = [Counter(prediction).most_common(1)[0][0] for prediction in predictions]
 
-        total_images +=1
+    ground_truth_label = os.path.basename(root)
+    correct_predictions += sum([1 for final_pred in final_predictions if final_pred == ground_truth_label])
+    total_images += len(final_predictions)
 
-        # Print the final prediction for the image
+    # Print the final prediction for each image
+    for image_file, final_pred in zip(files, final_predictions):
         print(f"Image: {image_file}")
-        print(f"Final Prediction: {final_prediction}")
+        print(f"Final Prediction: {final_pred}")
         print("---------------------------")
 
-    accuracy = correct_predictions / total_images
-    print(f"Accuracy: {accuracy * 100:.2f}%")
+accuracy = correct_predictions / total_images
+print(f"Accuracy: {accuracy * 100:.2f}%")
 
 print("Prediction process completed.")
